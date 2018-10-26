c46e6ebfc80bd5d6a48ef83a2d7976a6a9921971
Richard Wu <Richie1988@gmail.com>
2018-10-26 11:50:44 -0400

    more userinput

--- morningexercise.10/main.js
+++ morningexercise.10/main.js
@@ -6,8 +6,8 @@ console.log('linked');
 // gridContainer[0].style.gridTemplate='150px 150px 150px/150px 150px 150px';
 
 var grid = document.getElementById('gridcontainer');
-var rows = prompt('how many rows?');
 var cols = prompt('how many columns?');
+var rows = prompt('how many rows?');
 var dm = [rows,cols];
 
 for (let i=0;i<dm[0]*dm[1];i++){
