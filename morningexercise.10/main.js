b9e5e4eb546e1b88ff43e1622259bb8bb9db0efe
Richard Wu <Richie1988@gmail.com>
2018-10-26 10:27:54 -0400

    first commit

---
+++ morningexercise.10/main.js
@@ -0,0 +1,6 @@
+console.log('linked');
+
+var gridContainer = document.getElementsByClassName('gridcontainer');
+// console.log(gridContainer);
+gridContainer[0].style.display='grid';
+gridContainer[0].style.gridTemplate='150px 150px 150px/150px 150px 150px';
