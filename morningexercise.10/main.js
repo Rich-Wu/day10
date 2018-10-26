cd663854bfb730c986fe15c45051cf91d94bb3db
Richard Wu <Richie1988@gmail.com>
2018-10-26 13:57:09 -0400

    added eventListener examples

--- morningexercise.10/main.js
+++ morningexercise.10/main.js
@@ -13,6 +13,7 @@ for (let i=0;i<dm[0]*dm[1];i++){
   // console.log(div);
   grid.appendChild(div);
   div.style.backgroundImage = "url('profile_thumb.jpg')";
+  div.setAttribute('id','image-'+(i+1));
   // let image = document.createElement('img');
   // div.appendChild(image);
   // // console.log(image);
@@ -22,3 +23,14 @@ for (let i=0;i<dm[0]*dm[1];i++){
 grid.style.display = 'grid';
 grid.style.gridTemplate = `repeat(${dm[0]},100px)/repeat(${dm[1]},100px)`;
 grid.style.gridGap = '3px';
+
+function showMessage() {
+  alert('You clicked an image');
+}
+
+function lightsOff() {
+  document.body.style.backgroundColor = 'black';
+  document.body.style.color = 'white';
+}
+var fav = document.getElementById('image-5');
+fav.addEventListener('dblclick',lightsOff);
