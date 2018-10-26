bd1ea25508b38218dc4fa0f3ee3126c47ac8f43a
Richard Wu <Richie1988@gmail.com>
2018-10-26 11:35:44 -0400

    used for loop solution

--- morningexercise.10/main.js
+++ morningexercise.10/main.js
@@ -1,6 +1,22 @@
 console.log('linked');
 
-var gridContainer = document.getElementsByClassName('gridcontainer');
-// console.log(gridContainer);
-gridContainer[0].style.display='grid';
-gridContainer[0].style.gridTemplate='150px 150px 150px/150px 150px 150px';
+// var gridContainer = document.getElementsByClassName('gridcontainer');
+// // console.log(gridContainer);
+// gridContainer[0].style.display='grid';
+// gridContainer[0].style.gridTemplate='150px 150px 150px/150px 150px 150px';
+
+var grid = document.getElementById('gridcontainer');
+var dm = [3,3];
+
+for (let i=0;i<dm[0]*dm[1];i++){
+  let div = document.createElement('div');
+  // console.log(div);
+  grid.appendChild(div);
+  let image = document.createElement('img');
+  div.appendChild(image);
+  // console.log(image);
+  image.src = 'profile_thumb.jpg';
+}
+
+grid.style.display = 'grid';
+grid.style.gridTemplate = `repeat(${dm[0]},150px)/repeat(${dm[1]},150px)`;
