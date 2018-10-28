16766a7581e2842c03eaac0374c829caef72503b
Richard Wu <Richie1988@gmail.com>
2018-10-27 21:19:02 -0400

    wrote fadeIn and fadeOut function

--- workshop.10/kindle-michael.js
+++ workshop.10/kindle-michael.js
@@ -5,21 +5,31 @@ function idP(){
   }
 }
 
-function fadeIn() {
-  let posY = window.scrollY;
-  let vpHeight = window.innerHeight;
-  for (i=0;i<parasNodes.length;i++) {
-      if (window.scrollY > (vpHeight*0.5*i)+(vpHeight*0.1)){
-        // console.log(vpHeight+' '+posY+' '+i);
-        // console.log((vpHeight*i)+(vpHeight*0.3));
-        document.getElementById('para-'+i).style.opacity = '1';
-        document.getElementById('para-'+i).style.transition = 'opacity 1s';
-      }
-      // if (posY > )
-  }
+// function fadeIn() {
+//   // let posY = window.scrollY;
+//   // let vpHeight = window.innerHeight;
+//   // for (i=0;i<parasNodes.length;i++) {
+//       if (){
+//         // console.log(vpHeight+' '+posY+' '+i);
+//         // console.log((vpHeight*i)+(vpHeight*0.3));
+//         document.getElementById('para-'+i).style.opacity = '1';
+//         document.getElementById('para-'+i).style.transition = 'opacity 1s';
+//       }
+//       // if (posY > )
+//   // }
+//
+// }
 
+idP();
+var parasNodes = document.getElementsByTagName('p');
+var singleParaTest = document.getElementById('para-2');
+function fadeIn(paraId) {
+  document.getElementById(paraId).style.opacity = '1';
+  document.getElementById(paraId).style.transition = 'opacity 1s';
+}
+function fadeOut(paraId) {
+  document.getElementById(paraId).style.opacity = '0';
+  document.getElementById(paraId).style.transition = 'opacity 1s';
 }
 
-var paras = idP();
-var parasNodes = document.getElementsByTagName('p');
-window.addEventListener('scroll',fadeIn);
+// window.addEventListener('scroll',fadeIn);
